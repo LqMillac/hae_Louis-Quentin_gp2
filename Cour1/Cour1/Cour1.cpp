28b08fc9a97700a905c6272a72ac7637449e9d9e
Millac Louis-Quentin <lq.millac@hotmail.com>
2019-10-04 10:33:44 +0200

    le chat

---
+++ Cour1/Cour1/Cour1.cpp
@@ -0,0 +1,87 @@
+
+
+#include "pch.h"
+#include <iostream>
+
+int StrChr(char*grange, char chat)
+{
+	for (int i = 0; grange[i] !=0 ; i++)
+	{
+		if (grange[i] == chat)
+		{
+			return i;
+		}
+ 
+	}
+	return -1;
+
+}
+int StrChr2(char*grange, char chat)
+{
+	int i = 0;
+	
+	while (grange[i]!=0)
+	{
+	
+		if (grange[i] == chat)
+		{
+
+			return i;
+		}
+		i++;
+	}
+	return-1;
+}
+int StrChr3(char*grange, char chat)
+{
+	int length = strlen(grange);
+	for (int i = 0; i < length; i++)
+	{
+		return i;
+	}
+	return-1;
+}
+
+
+//void Memcpy(char * dest, char * src, int size) 
+//{
+//	for (int i = 0; i< size; i++)
+//	{
+//		dest[i] = src[i];
+//	}
+//
+//
+//}
+void Memcpy2(char * dest, char * src, int size)
+{
+	while (size <= -1)
+	{
+		dest[size] = src[size];
+		size--;
+	}
+
+	// AUTRE SOLUTION 
+	// while (size--)
+	//tant que (valeur sur la pile ) 
+	// laisser la valeur de size sur la pile 
+	// faire le tant que 
+	// décremente la valeur de size 
+}
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+
+	char licorne[32] = "licorne";
+	char vomi[32] = "vomit";
+	//Memcpy(licorne, vomi,strlen(licorne)+1);
+	Memcpy2(licorne, vomi, strlen(licorne) + 1);
+
+	printf("licorne %s\n", licorne);
+
+
+	char chateau[32]= "chateau";
+	char soldat = 'e';
+	int posSoldat = StrChr(chateau, soldat);
+	printf("le chateau est en position %d\n", posSoldat);
+}
