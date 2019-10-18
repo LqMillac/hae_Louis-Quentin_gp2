b69d151ba1b2b1e3af55d6c83b1897e5fb61c704
Millac Louis-Quentin <lq.millac@hotmail.com>
2019-10-18 17:28:49 +0200

    bfs

--- Cour3/cour3/cour3/cour3.cpp
+++ Cour3/cour3/cour3/cour3.cpp
@@ -3,13 +3,45 @@
 #include "IntArray.hpp"
 #include "Util.hpp"
 #include "List.hpp"
+#include "Tree.hpp"
 
 void assert(bool b) {
 	if (!b) throw std::exception();
 }
+
+void TestIntTree() {
+	IntTree * tree = new IntTree();
+	Node<int> * leaf = new IntNode(8);
+	leaf->insert(4);
+	//assert(leaf->left->elem == 4);
+
+	leaf->insert(9);
+	//assert(leaf->right->elem == 9);
+
+	leaf->insert(16);
+	leaf->insert(3);
+	leaf->insert(5);
+	assert(leaf->getLength() == 6);
+
+
+	assert(leaf->Contains(16) == true);
+	leaf->remove(16);
+	assert(leaf->Contains(16) == false);
+	assert(leaf->Contains(4) == true);
+	leaf->remove(4);
+	assert(leaf->Contains(4) == false);
+	leaf->insert(0);
+	assert(leaf->Contains(0) == true);
+	leaf->remove(0);
+	assert(leaf->Contains(0) == false);
+
+	int k = 0;
+	leaf->dfsPrint();
+	leaf->dfsPrintReverse();
+}
 void testIntList()
 {
-	IntList *LaListe = new IntList(8);
+	/*IntList *LaListe = new IntList(8);
 	int toto = 0;
 	LaListe->Append(65);
 	assert(LaListe->Length() == 2);
@@ -22,11 +54,21 @@ void testIntList()
 	assert(LaListe->Length() == 3);
 	LaListe = LaListe->Remove(33);
 	assert(LaListe->Length() == 2);
-	int k = 0;
+	
+
+	bool LaReponse = LaListe->Contains(60);
+	assert(LaReponse == false);
+
+	bool LaReponse2 = LaListe->Contains(65);
+	assert(LaReponse2 == true);
+
+	int k = 0;*/
 }
 int main()
 {
 	testIntList();
+	TestIntTree();
+	
 	
 
 	/*const int len = 4;
