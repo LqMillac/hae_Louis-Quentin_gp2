8d85b32f29242896ce89722c07642239a5dee8bb
Millac Louis-Quentin <lq.millac@hotmail.com>
2019-10-18 13:42:09 +0200

    bsearch fini

--- Cour3/cour3/cour3/IntArray.cpp
+++ Cour3/cour3/cour3/IntArray.cpp
@@ -3,6 +3,28 @@
 
 int IntArray::TEST = 66;
 
+IntArray::IntArray(int size, const char * name) {
+	//on passe name en std::string
+	//alloue automatiquement un char* et fait la copie
+	this->name = name;
+	printf("construite! %s\n", this->name.c_str());
+
+	//version C old school
+	//data = (int * )malloc(size * sizeof(int));
+	//memset(data,0, size * sizeof(int));
+
+	//version C new school
+	//data = (int * )calloc(size , sizeof(int));
+	//calloc mets des zero
+	if (size == 0) size++;
+	//version c++ plus safe
+	data = new int[size];
+	for (int i = 0; i < size; ++i) data[i] = 0;
+
+	//cursize = 0;
+	maxSize = size;
+}
+
 bool IntArray::ensure(int size) {
 	if (size < maxSize) return false;
 
@@ -20,70 +42,41 @@ bool IntArray::ensure(int size) {
 	return true;
 }
 
-  void IntArray::push_back(int elem)
- {
-	 ensure(curSize + 1);
-	 data[curSize] = elem;
-	 curSize++;
-
- }
-
- void IntArray::push_front(int elem)
- {
-	 ensure(curSize + 1);
-	 for (int i = curSize; i > 0; i--)
-	 {
-		 data[i] = data[i - 1];
-	 }
-	 data[0] = elem;
-	 curSize++;
- }
-
-void IntArray::insert(int pos, int elem)
- {
-	 ensure((pos >= curSize) ? (pos + 1) : (curSize + 1));
-
-	 /* if (( pos >=curSize))
-	 {
-	 ensure(pos+1);
-	 else 
-	 ensure (curSize +1);
-	 }
-	 */
-	 for (int i = curSize; i > pos; i--)
-	 {
-		 data[i] = data[i - 1];
-	 }
-	 data[pos] = elem;
-	 if (pos >= curSize)
-		 curSize = pos + 1;
-	 else
-		 curSize++;
+int IntArray::getLength() {
+	return curSize;
+}
 
 
- }
+void IntArray::push_back(int elem) {
+	ensure(curSize + 1);
+	data[curSize] = elem;
+	curSize++;
+}
 
-/*void IntArray::sort()
-{
-	IntArray nuData(5, "nudata");
-	for (int i = 0; i < getLength()-1; i++)
-	{
-		int val = get(i);
-		printf("val : %d\n", val);
-		int pos = nuData.searchposition(val);
-		printf("future pos : %d\n", pos);
-		nuData.insert(nuData.searchposition(val), val);
-		printf("[");
-		for (int j = 0; j < nuData.getLength()-1; j++)
-		{
-			printf("%d ", nuData[j]);
-		}
-		printf("]\n");
+void IntArray::push_front(int elem) {
+	ensure(curSize + 1);
+	for (int i = curSize; i > 0; i--)
+		data[i] = data[i - 1];
+	data[0] = elem;
+	curSize++;
+}
 
-	}
-	for (int i = 0; i < getLength(); i++)
-	{
-		set(i, nuData.get(i));
-	}
-	int i = 0;
-}*/
+void IntArray::insert(int pos, int elem) {
+	/**
+	if( ( pos>=curSize ) ){
+		ensure(pos+1);
+	else
+		ensure( curSize +1 );
+	*/
+	ensure((pos >= curSize) ? (pos + 1) : (curSize + 1));
+
+	for (int i = curSize; i > pos; i--)
+		data[i] = data[i - 1];
+
+	data[pos] = elem;
+
+	if (pos >= curSize)
+		curSize = pos + 1;
+	else
+		curSize++;
+}
