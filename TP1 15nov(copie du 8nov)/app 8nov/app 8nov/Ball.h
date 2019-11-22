f7f94f4d82f62891e760cf9cb66095716f8241cf
Louis-Quentin Millac <lq.millac@hotmail.com>
2019-11-22 09:11:13 +0100

    ball h

---
+++ TP1 15nov(copie du 8nov)/app 8nov/app 8nov/Ball.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+class Ball
+{
+	
+	
+public:
+	sf::Vector2f position;
+	sf::CircleShape ball;
+	sf::CircleShape BallCollider;
+	Ball(sf::Vector2f pos,float size)
+	{
+		ball.setRadius(size);
+		position = pos;
+		ball.setFillColor(sf::Color::Red);
+		ball.setPosition(pos);
+
+
+	}
+	void SetPosition()
+	{
+		ball.setPosition(position);
+	}
+
+	
+};
+
